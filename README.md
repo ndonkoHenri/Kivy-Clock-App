10bf483fb489924a43e7296e8bdfcf92a55a4a98
ndonkoHenri <robotcoder4@protonmail.com>
2022-07-31 05:51:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Basic Clock and Chrono Application using Kivy/KivyMD
+
+This application was built using [Kivy](https://github.com/kivy/kivy) (a python framework for building cross-platform apps) and [KivyMD](https://github.com/kivymd/KivyMD) library.
+
+The app's interface is quite simple and straight-forward for any user.
+
+#### When executed, three main parts are observed:
+
+- A digital clock (with the same time as the device running the program);
+- A Stopwatch
+- Two Buttons for the Stopwatch(a start/end Button and a reset button) both self-explanatory;
+
+
+This App is still under development, and I will be opened to respond to any issue or checkout any pull-requests.
+
+You can follow me or add a Watch to stay updated.
+
+
---
+++ clock_app.py
@@ -0,0 +1,94 @@
+from time import strftime
+from kivy import Config
+
+Config.set("graphics", "height", "323")
+Config.set("graphics", "width", "444")
+
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivy.clock import Clock
+
+
+# TODO: Find how to unschedule events from the MainScreen (self.root.parent.blablabla)
+# TODO: Write the functions found in the KV file, here in the pythonFile
+
+
+class ClockApp(MDApp):
+    # Clock object for the Chrono event
+    event_chrono = None
+    # Clock object for the clock(in the app) event
+    event_clock = None
+
+    def build(self):
+        """
+        The build function is the main entry point for the build system. It
+        creates a new Builder object.
+        :return: A MainScreen Object containing the main screen to the app
+        """
+        return MainScreen()
+
+    def on_start(self):
+        """
+        The on_start function is called when the app is first opened. It creates a
+        Clock event that calls the root widget's clock_update and chrono_update functions to
+        update the clock and chrono in real time.
+        """
+
+        self.event_clock = Clock.schedule_interval(self.root.clock_update, 1)
+        self.event_chrono = Clock.schedule_interval(self.root.chrono_update, 0.032)
+
+
+class MainScreen(MDScreen):
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.chrono_mins = 0
+        self.chrono_secs = 0
+        self.total_seconds = 0
+
+    def clock_update(self, dt):
+        """
+        The clock_update function updates the time of the Clock in the app.
+        This function is called once every second.
+
+        :param dt: the time delta, which is approximately equal to the specified time-out (1sec in this case)
+        """
+        self.time_label.text = strftime('[b] %H [/b]: %M : %S %p')
+
+    def chrono_update(self, dt):
+        """
+        The chrono_update function calculates the chrono_mins(number of mins since started)
+        and chrono_secs(number of seconds since started) variables from the sum of ~param: dt (total_seconds)
+        and then updates the text of Chrono in the app to reflect these changes.
+
+        :param dt: Update the chrono_mins and chrono_secs variables
+        """
+        self.total_seconds += dt
+        self.chrono_mins, self.chrono_secs = divmod(self.total_seconds, 60)
+        self.ids.chrono_label.text = f"[b] {int(self.chrono_mins)} [/b]: {int(self.chrono_secs)}.[size=30sp]{int((self.chrono_secs * 100) % 100)}[/size]"
+
+    def reset_chrono(self):
+        """
+        Resets the Chrono and changes the start_stop_btn.text to "Start"
+        """
+
+    def start_stop_chrono(self):
+        """
+        Starts or Stops the Chrono depending on the Text in the Button(start_stop_btn)
+        and equally changes it(the text) accordingly
+        """
+
+
+if __name__ == "__main__":
+    from kivy.core.text import LabelBase
+
+    # Register the different font files(.ttf) to the LabelBase with a specific font_name
+    # for easy referencing in both the .kv and .py files
+
+    LabelBase.register(
+        name="Roboto",
+        fn_regular="assets/fonts/Roboto-Thin.ttf",
+        fn_bold="assets/fonts/Roboto-Medium.ttf"
+    )
+    # Run the App
+    ClockApp().run()
